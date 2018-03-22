7ec6ac580958c284ac50ede3aabb254e29e9a0a8
ale96ametis <alessandro.ametis@gmail.com>
2018-03-22 11:28:05 +0100

    Sistemata interfaccia e cambiato sistema di analisi

--- video_record.py
+++ video_record.py
@@ -23,7 +23,7 @@ class VideoRecorder():
 		
 		self.open = True
 		self.device_index = 0
-		self.fps = 8               # fps should be the minimum constant rate at which the camera can
+		self.fps = 6               # fps should be the minimum constant rate at which the camera can
 		self.fourcc = "MJPG"       # capture images (with no decrease in speed over time; testing is required)
 		self.frameSize = (640,480) # video formats and sizes also depend and vary according to the camera used
 		self.video_filename = name_video
@@ -37,11 +37,13 @@ class VideoRecorder():
 	
 	# Video starts being recorded 
 	def record(self):
-		
+		global recording
+		recording = True
 #		counter = 1
 		timer_start = time.time()
 		timer_current = 0
-		
+		detector = dlib.get_frontal_face_detector()
+		predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 		
 		while(self.open==True):
 			ret, video_frame = self.video_cap.read()
@@ -49,19 +51,34 @@ class VideoRecorder():
 				
 				self.video_out.write(video_frame)
 				self.frame_counts += 1
-				time.sleep(0.125)
-				
-				# Uncomment the following three lines to make the video to be
-				# displayed to screen while recording
-				img = Image.fromarray(video_frame, mode='RGB')
+
+				frame = imutils.resize(video_frame, width=400)
+				gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+				faces = detector(gray, 0)
+				#loop over the face detections
+				for face in faces:
+					# determine the facial landmarks for the face region, then
+					# convert the facial landmark (x, y)-coordinates to a NumPy
+					# array
+					shape = predictor(gray, face)
+					shape = face_utils.shape_to_np(shape)
+					# loop over the (x, y)-coordinates for the facial landmarks
+					# and draw them on the image
+					for (x, y) in shape:
+						cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
+
+				frame = cv2.resize(frame, (640,480))
+				img = Image.fromarray(frame, mode='RGB')
 				img = img.resize((500,400))
 				imgtk = ImageTk.PhotoImage(img)
 				lbl.config(image=imgtk)
 				lbl.img = imgtk
+				#cv2.imshow('frame',frame)
 				"""
 				cv2.imshow('video_frame', video_frame)
 				cv2.waitKey(1)	
 				"""
+				time.sleep(0.125)
 				# 0.16 delay -> 6 fps
 				#
 			else:
@@ -139,30 +156,38 @@ class AudioRecorder():
 		audio_thread.start()
 
 def start_AVrecording():
-	
 	global video_thread
 	global audio_thread
+	if (recording==False):
+		video_thread = VideoRecorder()
+		audio_thread = AudioRecorder()
 
-	video_thread = VideoRecorder()
-	audio_thread = AudioRecorder()
-
-	audio_thread.start()
-	video_thread.start()
+		audio_thread.start()
+		video_thread.start()
 
 	return
 
 def stop_AVrecording():
 	global ntrial
 	global name_video
-	print("Stopping recording")
-	audio_thread.stop()
-	video_thread.stop()
-	landmarks()
-	muxing()
-	file_manager()
-	ntrial = ntrial+1
-	name_video = 'output_phrase[%d]_[%d].avi' %(num_phrase, ntrial)
-	print('[INFO]Video saved')
+	global recording
+	global nread
+	if (recording==True):
+		print("Stopping recording")
+		audio_thread.stop()
+		video_thread.stop()
+		landmarks()
+		muxing()
+		file_manager()
+		ntrial = ntrial+1
+		name_video = 'output_phrase[%d]_[%d].avi' %(num_phrase, ntrial)
+		print('[INFO]Video saved')
+		recording = False
+		result['text'] = "Premere Play e leggere una seconda volta per l'analisi"
+	if (nread == 1):
+		analyse_marker()
+		nread = -1
+	nread+=1
 	return
 	
 def muxing():
@@ -171,7 +196,6 @@ def muxing():
 	#	time.sleep(1)
 
 	frame_counts = video_thread.frame_counts
-	#elapsed_time = time.time() - video_thread.start_time
 	elapsed_time = video_thread.end_time - video_thread.start_time
 	recorded_fps = frame_counts / elapsed_time
 	print("total frames " + str(frame_counts))
@@ -179,7 +203,7 @@ def muxing():
 	print("recorded fps " + str(recorded_fps))
 #	 Merging audio and video signal
 	
-	if abs(recorded_fps - video_thread.fps) >= 0.01:    # If the fps rate was higher/lower than expected, re-encode it to the expected
+	if abs(recorded_fps - 6) >= 0.01:    # If the fps rate was higher/lower than expected, re-encode it to the expected
 								
 		print("Re-encoding")
 		cmd = "ffmpeg -r " + str(recorded_fps) + " -i marker-"+name_video+" -pix_fmt yuv420p -r 6 temp_video2.avi"
@@ -226,11 +250,14 @@ def landmarks():
 				# and draw them on the image
 				mouth_marker = 0
 				for (x, y) in shape:
-					if (mouth_marker>47):
+					if (mouth_marker==48):
+						x_temp = x
+						y_temp = y
+					if (mouth_marker>48):
 						cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
-						out_file.write(str(x))
+						out_file.write(str(x-x_temp))
 						out_file.write("\n")
-						out_file.write(str(y))
+						out_file.write(str(y-y_temp))
 						out_file.write("\n")
 					mouth_marker += 1
 				out_file.write("1000\n")
@@ -244,7 +271,6 @@ def landmarks():
 			cap.release()
 			#cv2.destroyAllWindows()
 			break
-	analyse_marker(marker)
 	print('[INFO]End!')
 	return
 
@@ -266,17 +292,17 @@ def file_manager():
 	#	os.remove(str(local_path) + "/marker-"+name_video)
 
 	
-def analyse_marker(current_marker):
-	import numpy as np
-	base_marker = "base_marker/marker[%d].txt" %(num_phrase)
-	f1 = open(base_marker,"r")
-	f2 = open(current_marker,"r")
+def analyse_marker():
+	first_trial = 'marker[%d]_[%d].txt' %(num_phrase, ntrial-2)
+	second_trial = 'marker[%d]_[%d].txt' %(num_phrase, ntrial-1)
+	f1 = open(first_trial,"r")
+	f2 = open(second_trial,"r")
 
-	#global testo1
-	#global testo2
+	global testo1
+	global testo2
 
-	n=100 #righe matrice
-	m=40 #colonne matrice
+	n=1000 #righe matrice
+	m=38 #colonne matrice
 
 	array1 = np.zeros((n,m)) #matrice per l'inserimento dei valori da file
 	array2 = np.zeros((n,m)) #matrice per l'inserimento dei valori da file
@@ -287,7 +313,7 @@ def analyse_marker(current_marker):
 	z=0 #indice per scorrere le righe dell'array di controllo
 
 	c=1 #indica quale blocco di "l" righe considerare, quindi se le prime 5 righe, le seconde cinque e via cosi, viene incrementato nel codice
-	l=5 #numero di righe da confrontare per fare la media dei valori
+	l=10 #numero di facce da confrontare per fare la media dei valori
 	conta=0 #conta quanti valori vengono inseriti in media_tot
 	media_tot=0
 
@@ -307,76 +333,60 @@ def analyse_marker(current_marker):
 			
 		#se raggiungo il carattere di controllo sono arrivato a "fine riga" dei marker
 		if int(testo1) == 1000: #testo=1000 = carattere di controllo che indice il fine riga dei marker
-			#passo alla riga successiva
+			#passo alla riga successiva e riporto a zero la colonna della matrice -> j=0
 			i = i+1
+			j = -1
 			#esco se sforo le dimensioni della matrice (posso ignorare gli ultimi marker)
 			if i == n:
 				break
-				
-			#se sono arrivato a fine riga marker riporto a zero la colonna della matrice -> j=0
-			j=0
-			#leggo i nuovi marker (quelli subito dopo il carattere di controllo)
-			testo1 = f1.readline()
+
+		if int(testo1) != 1000:
+			#salvo il contenuto dei file nelle matrici		
+			array1[i][j]=(int(testo1))
+			array2[i][j]=(int(testo2))
 			
-			#condizioni di uscita
-			if testo1 == "":
-				break
-			testo2 = f2.readline()
-			if testo2 == "":		
-				break
-		
-		#salvo il contenuto dei file nelle matrici		
-		array1[i][j]=(int(testo1))
-		array2[i][j]=(int(testo2))
-		
-		if i == c*l:	
-			while k<i:
-				while j<m:
-					#inserisco all'interno degli array di check la somma dei valori considerati
-					check1[z][j]=check1[z][j]+array1[k][j]
-					check2[z][j]=check2[z][j]+array2[k][j]
-					j=j+1
-				k=k+1
-				j=0
-				
-			while j<m:
-				if check1[z][j]==0:
-					break
-				#calcolo la media degli array di check
-				check1[z][j]=check1[z][j]/l
-				check2[z][j]=check2[z][j]/l
-				ris[z][j]=check1[z][j]-check2[z][j] #array con le medie dei diversi valori
-				media_tot=media_tot+abs(ris[z][j]) #media complessiva dell'array ris
-				conta=conta+1 #conta quanti valori sono stati inseriti in media_tot
-				j=j+1
-			z=z+1	
-			#riporto j a 0, verra' subito incrementata a 1 ma questo e' ok perche' ho gia' salvato array[i][0] (vedi sopra)
-			j=0
-			#incremento il controllo sulle righe da considerare
-			c=c+1	
-		j=j+1
-		
-	media_tot=media_tot/conta
+		j = j+1
 
-	#print (array1)
-	#print (array2)
-	#print (check1)
-	#print (check2)
-	#print (ris)
+	j=0
+	while k<i:
+		while j<m:
+			#inserisco all'interno degli array di check la somma dei valori considerati
+			check1[z][j]=check1[z][j]+array1[k][j]
+			check2[z][j]=check2[z][j]+array2[k][j]
+			j=j+1
+		k=k+1
+		c=c+1
+		j=0
+		if c == l:
+			z = z+1
+			c=1
+
+	j=0
+	z=0
+	while z<i:
+		while j<m:
+			#calcolo la media degli array di check
+			check1[z][j]=check1[z][j]/l
+			check2[z][j]=check2[z][j]/l
+			ris[z][j]=check1[z][j]-check2[z][j]
+			media_tot=media_tot+abs(ris[z][j]) #media complessiva dell'array ris
+			j=j+1
+		z=z+1
+		j=0
+
+	index = np.where(~ris.any(axis=1))[0]
+	new_ris = np.delete(ris, index, axis=0)
+
+	media_tot=media_tot/(len(new_ris)*n)
+
 	print ("media tot:",media_tot)
-
-	if media_tot < 5:
+	
+	if media_tot < 0.05:
 		print ("lettura corretta")
 		result['text'] = "lettura corretta"
-	if media_tot > 5 and media_tot < 10:
-		print ("lettura con possibilità di qualche piccolo errore")
-		result['text'] = "lettura con possibilità di qualche piccolo errore"
-	if media_tot > 10 and media_tot < 15:
-		print ("lettura con probabile presenza di errori")
-		result['text'] = "lettura con probabile presenza di errori"
-	if media_tot > 15:
-		print ("lettura contenente evidenti errori")
-		result['text'] = "lettura contenente evidenti errori"
+	else:
+		print ("lettura contenente errori")
+		result['text'] = "lettura contenente errori"
 
 	f1.close()
 	f2.close()
@@ -384,44 +394,55 @@ def analyse_marker(current_marker):
 
 
 if __name__== "__main__":
+	global recording
+	global num_phrase
+	global name_video
+	global ntrial
+	global nread
 
-	#Initialize Tkinter
-	win = Tk()
+	nread=0
+	recording = False
+	#num_phrase = randint(0,(len(myList)-1))
+	num_phrase = 3
+	ntrial = 0
+
 	#Reading file with phrases
 	f = open("frasi.txt","r")
 	myList = []
 	for line in f:
 		myList.append(line)
-	global num_phrase
-	#num_phrase = randint(0,(len(myList)-1))
-	num_phrase = 3
-	global ntrial
-	ntrial = 0
+
 	phrase = myList[num_phrase]
-	global name_video
 	name_video ='output_phrase[%d]_[%d].avi' %(num_phrase, ntrial)
+	
+	#Initialize Tkinter
+	win = Tk()
+	
 	print("[INFO]Video name: ", name_video)
+	print("[TESTO]: ", phrase)
+	
 	#Windows
 	win.geometry("800x600")
 	win.title("Recording app")
+	
 	#Frame
 	frm = Frame(win)
 	frm.pack(expand=True)
-	print("[TESTO]: ", phrase)
+
+	#Label text
+	result = Label(frm, text="Premere Play e leggere la frase. Ripetere la lettura per l'analisi dei marker", justify='center')
+	result.grid(row=1, column=1, columnspan=3)
 	#Label video
 	tmp_img = ImageTk.PhotoImage(Image.new('RGB',(500,400)))
 	lbl = Label(frm)
 	lbl.config(image=tmp_img)
 	lbl.img = tmp_img
-	lbl.grid(row=1, column=1,rowspan=3)
+	lbl.grid(row=2, column=1,rowspan=3)
 	#Label text
 	text = Label(frm, text=phrase, wraplength=200, justify='center')
-	text.grid(row=1, column=2, columnspan=4,rowspan=2)
-	#Label text
-	result = Label(frm, text="Leggi la frase", justify='center')
-	result.grid(row=3, column=2)
+	text.grid(row=2, column=2, columnspan=4,rowspan=2)
 	#Buttons
-	Button(frm, text='Play', command = start_AVrecording).grid(row=2, column=3)
-	Button(frm, text='Stop', command = stop_AVrecording).grid(row=2, column=4)
+	Button(frm, text='Play', command = start_AVrecording).grid(row=3, column=3)
+	Button(frm, text='Stop', command = stop_AVrecording).grid(row=3, column=4)
 
 	win.mainloop()
